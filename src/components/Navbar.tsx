0d6132fff202dce3ea386050433e9c7546c96b1c
manas-jain09 <112091116+manas-jain09@users.noreply.github.com>
2025-05-12 22:14:27 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           <img 
             src="/MainLogo.png" 
             alt="Ikshvaku Innovations Logo" 
-            className="h-15 w-auto"
+            className="h-5 w-auto"
           />
         </div>
         
