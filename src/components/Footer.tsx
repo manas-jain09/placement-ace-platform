b2167d7ed2e538a8a81be8945af6124854fb7d33
Varad Charholikar <103520139+varadc-2304@users.noreply.github.com>
2025-05-13 13:45:40 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,8 @@ import { Home, Youtube } from 'lucide-react';
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-gray-300">
-      <div className="mt-12 pt-8 border-t border-gray-800 w-full flex justify-center">
-        <div className="flex flex-col md:flex-row justify-between items-center w-full max-w-screen-lg">
-          <p className="text-sm text-center">© 2025 Ikshvaku Innovations. All rights reserved.</p>
-          <div className="mt-4 md:mt-0"></div>
-        </div>
+      <div className="flex flex-col justify-center items-center w-full max-w-screen-lg py-8">
+        <p className="text-sm text-center">© 2025 Ikshvaku Innovations. All rights reserved.</p>
       </div>
     </footer>
   );
